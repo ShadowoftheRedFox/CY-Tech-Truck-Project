560cdc3589a41b9634be6229469d6dc1cda40365
ShadowoftheRedFox <leon.merin@gmail.com>
2023-12-03 09:57:30 +0100

    gives the arguments to the shell script

--- BashRunnerWindows.bat
+++ BashRunnerWindows.bat
@@ -1,3 +1,4 @@
 @echo off
 @REM If you have mysys64 installed, it has a shell runner for windows
-C:\msys64\msys2_shell.cmd -defterm -no-start -mingw64 -here .\CYTruck.sh
+@REM %* is to add all argument to the shell script if they are passed from windows
+C:\msys64\msys2_shell.cmd -defterm -no-start -mingw64 -here .\CYTruck.sh %*
